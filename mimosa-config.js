e1cfe9df73e93accbc8c073731d9a11441a0069e
david.bashford <dbashford@bericotechnologies.com>
2015-01-24 13:17:02 -0500

    variable stuff into build

--- mimosa-config.js
+++ mimosa-config.js
@@ -14,6 +14,10 @@ exports.config = {
         },
         contexts: {
           about: {
+            title:"FAQ/Tips - Mimosa",
+            desc:"Common questions and issues.",
+            header:"FAQ & Mimosa Tips",
+            lead:"Questions answered & tricks discussed"
           },
           commands: {
             title:"Commands - Mimosa",
@@ -22,18 +26,30 @@ exports.config = {
             lead:"Interact with your project via the command line"
           },
           configuration: {
-
+            title:"Configuration - Mimosa",
+            desc:"While preferring as little config as possible, Mimosa is hugely configurable.",
+            header:"Configuration",
+            lead:"Configuration explained & Mimosa core's configuration options"
           },
           extend: {
-
-          },
-          index: {
-
+            title:"How to Build Mimosa Modules - Mimosa",
+            desc:"Mimosa is extensible, making it easy to add your own functionality via modules.",
+            header:"Building A Mimosa Module",
+            lead:"Plug custom functionality into Mimosa"
           },
           modules: {
-
+            title:"Modules - Mimosa",
+            desc:"Mimosa is pluggable so new functionality can be added.",
+            header:"Modules",
+            lead:"Extend Mimosa's default functionality"
           },
           started: {
+            title:"Get Started - Mimosa",
+            desc:"Getting started is super easy, you'll be coding in no time.",
+            header:"Get Started",
+            lead:"Installation and project setup. You'll be coding in minutes."
+          },
+          index: {
 
           }
         }
